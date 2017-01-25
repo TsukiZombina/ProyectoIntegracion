57282797185f9f724387b9d404665fd0485464b6
TsukiZombina <ammy_zmmn@hotmail.com>
2017-01-24 23:56:48 -0600

    Initial Commit

---
+++ ProyectoIntegracion/Coordinate.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Coordinate.h
+ * Author: ammy_
+ *
+ * Created on January 24, 2017, 11:55 PM
+ */
+
+#ifndef COORDINATE_H
+#define COORDINATE_H
+
+class Coordinate {
+public:
+    Coordinate();
+    Coordinate(const Coordinate& orig);
+    virtual ~Coordinate();
+private:
+
+};
+
+#endif /* COORDINATE_H */
+
