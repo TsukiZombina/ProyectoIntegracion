7ea7de9690bf7763e92dfe20e50c18a7d65bfd13
TsukiZombina <ammy_zmmn@hotmail.com>
2017-01-25 00:53:18 -0600

    Initial Commit

---
+++ Proyecto de Integracion/Map.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Map.cpp
+ * Author: ammy_
+ * 
+ * Created on January 24, 2017, 11:54 PM
+ */
+
+#include "Map.h"
+
+Map::Map() {
+}
+
+Map::Map(const Map& orig) {
+}
+
+Map::~Map() {
+}
+
